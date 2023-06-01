34d101eeabe5ad7809f7b535ebce1675bc0f2359
builde7b0b <ggives3@gmail.com>
2023-06-01 16:10:47 -0700

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryManagementComponent } from './inventory-management/inventory-management.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryManagementComponent,
+    DashboardComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
