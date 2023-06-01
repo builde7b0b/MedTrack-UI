f00ab9931aa54b34c3d4d1b8efb8f1a52fd98b7d
builde7b0b <ggives3@gmail.com>
2023-06-01 16:56:25 -0700

    add styling to menu button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {MatToolbarModule } from '@angular/material/toolbar'
+import { MatMenuModule } from '@angular/material/menu';
 
 import {MatIconModule } from '@angular/material/icon'
 
@@ -25,6 +26,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatMenuModule,
     MatIconModule,
   ],
   providers: [],
