0d1eeb3a98ac4b397568506feb1dfded20407ff6
builde7b0b <ggives3@gmail.com>
2023-06-01 17:48:51 -0700

    add styling and branding colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {MatToolbarModule } from '@angular/material/toolbar'
 import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 import {MatIconModule } from '@angular/material/icon'
 
@@ -12,6 +13,7 @@ import { InventoryManagementComponent } from './inventory-management/inventory-m
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ReportsComponent } from './reports/reports.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -19,14 +21,18 @@ import { HomeComponent } from './home/home.component';
     InventoryManagementComponent,
     DashboardComponent,
     ReportsComponent,
-    HomeComponent
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatMenuModule,
+    MenuComponent,
+   
     MatIconModule,
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  
 })
 export class HomeComponent {
+  
 
 }
