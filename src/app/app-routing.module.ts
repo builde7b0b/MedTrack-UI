55e2553eddacdca2da9fd167c99f6399204f84ae
builde7b0b <ggives3@gmail.com>
2023-06-02 13:45:29 -0700

    add reports, dashboard routes and test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HomeComponent} from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MenuComponent } from './menu/menu.component';
-
+import { RolesComponent } from './roles/roles.component';
+import { ReportsComponent } from './reports/reports.component';
+import { InventoryManagementComponent } from './inventory-management/inventory-management.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'reports', component: ReportsComponent},
+  { path: 'dashboard', component: InventoryManagementComponent}
+
 ];
 
 @NgModule({
