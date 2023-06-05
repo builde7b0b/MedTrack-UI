36b5f8cb9530097270e0d2c5abce1fab81e9554d
builde7b0b <ggives3@gmail.com>
2023-06-05 14:37:31 -0700

    implmement service

--- src/app/medication.service.ts
+++ src/app/medication.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicationService {
+  private baseUrl = 'http://localhost:9092';
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  createMedication(){
+
+  }
+
+  getMedication(medicationId: number){
+    return this.http.get(`${this.baseUrl}/${medicationId}`)
+  }
 }
