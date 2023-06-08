fe842d7ac77e33b7b49db38edcdcf60510707986
builde7b0b <ggives3@gmail.com>
2023-06-08 08:46:17 -0700

    implment class and start css

--- src/app/Public/footer/footer.component.ts
+++ src/app/Public/footer/footer.component.ts
@@ -5,8 +5,25 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations: [
+    trigger('footerAnimation', [
+      state('void', style({ opacity: 0})),
+      transition('void <=> *', animate(500)),
+    ])
+  ]
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  state: string = 'default';
+
+  constructor() { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onIconClick(icon: string): void {
+    console.log('Clicked on ${icon}');
+    //add some logic
+  }
 
 }
